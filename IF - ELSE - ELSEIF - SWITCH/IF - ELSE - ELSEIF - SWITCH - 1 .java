b8f080f39506819414c0ae82533a1afb55d0acca
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 22:20:22 +0200

    Create IF - ELSE - ELSEIF - SWITCH - 1 .java

---
+++ IF - ELSE - ELSEIF - SWITCH/IF - ELSE - ELSEIF - SWITCH - 1 .java
@@ -0,0 +1,33 @@
+
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int num_rooms, price =0 , duplex;
+	System.out.println("enter the num of rooms (3 - 4 - 5)");
+	num_rooms=s.nextInt();
+
+	switch (num_rooms)
+	{
+	case 3:
+		price = 120;
+		break;
+	case 4:
+		price = 150;
+		break;
+	case 5:
+		System.out.println("is duplex? ( 1 - yes/ 0 - no) :");
+		duplex=s.nextInt();
+		if (duplex == 1) {
+			price = 200;
+		}
+		else {
+			price = 180;
+			break;
+		}
+
+	}
+	System.out.println("please py me"  +price +" nis");
+
+
+	}
+
+}
